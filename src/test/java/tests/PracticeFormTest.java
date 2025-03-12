83e0b9a55f868c774b87158bc9c6ff77a8c422dd
Oana-Niga <151163536+Oana-Niga@users.noreply.github.com>
2025-03-12 17:20:23 +0200

    Am aplicat polimorfismul static

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -41,15 +41,16 @@ public class PracticeFormTest extends SharedData {
         elementHelper.fillLocator(emailElement, emailValue);
 
         String genderValue = "Female";
-        List<WebElement> genderOptionsList = driver.findElements(By.xpath("//input[@name='gender']"));
+        By genderOptionsElement = By.xpath("//input[@name='gender']");
+        List<WebElement> genderOptionsList = driver.findElements(genderOptionsElement);
         if (genderValue.equals("Male")) {
-            executor.executeScript("arguments[0].click();", genderOptionsList.get(0));
+            elementHelper.clickJSLocator(genderOptionsList.get(0));
         }
         if (genderValue.equals("Female")) {
-            executor.executeScript("arguments[0].click();", genderOptionsList.get(1));
+            elementHelper.clickJSLocator(genderOptionsList.get(1));
         }
         if (genderValue.equals("Other")) {
-            executor.executeScript("arguments[0].click();", genderOptionsList.get(2));
+            elementHelper.clickJSLocator(genderOptionsList.get(2));
         }
 
         WebElement mobilePhoneElement = driver.findElement(By.xpath("//input[@placeholder='Mobile Number']"));
